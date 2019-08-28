d29398ff7d57872a7f2eddfbe0b28127afabbcd4
petraartep <petra.pogacsas@gmail.com>
2019-08-28 21:34:51 +0100

    register

--- Gemfile
+++ Gemfile
@@ -4,8 +4,6 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
-ruby "2.6.3"
-
 gem 'rake', '~> 11.2', '>= 11.2.2'
 gem 'sinatra', '~> 1.4', '>= 1.4.7'
 
@@ -14,6 +12,6 @@ group :test do
   gem 'coveralls', '~> 0.8.15', require: false
   gem 'rspec', '~> 3.5'
   gem 'rubocop', '~> 0.56.0'
-  gem 'simplecov', '~> 0.12.0', require: false
-  gem 'simplecov-console', '~> 0.4.1', require: false
+  gem 'simplecov', require: false
+  gem 'simplecov-console', require: false
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,14 @@
+ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+require 'capybara'
 require 'capybara/rspec'
+require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
+
+Capybara.app = RPS
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
@@ -26,6 +34,7 @@ SimpleCov.start
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  config.include Capybara::DSL
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
